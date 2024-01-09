71ef57792676d455929c2dd850cd51c062407c93
Brian <bwook9908@gmail.com>
2024-01-09 06:00:02 +0000

    feat: alarm clock implemented

--- threads/thread.c
+++ threads/thread.c
@@ -27,6 +27,8 @@
 /* List of processes in THREAD_READY state, that is, processes
    that are ready to run but not actually running. */
 static struct list ready_list;
+static struct list sleep_list;
+static int64_t next_tick_to_awake;
 
 /* Idle thread. */
 static struct thread *idle_thread;
@@ -108,6 +110,7 @@ thread_init (void) {
 	/* Init the globla thread context */
 	lock_init (&tid_lock);
 	list_init (&ready_list);
+	list_init (&sleep_list);
 	list_init (&destruction_req);
 
 	/* Set up a thread structure for the running thread. */
@@ -131,6 +134,49 @@ thread_start (void) {
 
 	/* Wait for the idle thread to initialize idle_thread. */
 	sema_down (&idle_started);
+}
+
+
+void update_next_tick_to_awake(int64_t ticks){
+    next_tick_to_awake = (next_tick_to_awake > ticks) ? ticks : next_tick_to_awake;
+}
+
+int64_t get_next_tick_to_awake(void){
+    return next_tick_to_awake;
+}
+
+void thread_sleep(int64_t ticks) {
+    struct thread *cur;
+
+    enum intr_level old_level;
+    old_level = intr_disable();
+    cur = thread_current();
+    ASSERT(cur != idle_thread);
+
+    update_next_tick_to_awake(cur-> wakeup_tick = ticks);
+
+    list_push_back(&sleep_list, &cur->elem);
+
+    thread_block();
+
+    intr_set_level(old_level);
+}
+
+void thread_awake(int64_t wakeup_tick){
+    next_tick_to_awake = INT64_MAX;
+    struct list_elem *e;
+    e = list_begin(&sleep_list);
+    while(e != list_end(&sleep_list)){
+        struct thread * t = list_entry(e, struct thread, elem);
+
+        if(wakeup_tick >= t->wakeup_tick){
+            e = list_remove(&t->elem);
+            thread_unblock(t);
+        }else{
+            e = list_next(e);
+            update_next_tick_to_awake(t->wakeup_tick);
+        }
+    }
 }
 
 /* Called by the timer interrupt handler at each timer tick.
