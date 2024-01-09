10995ca646602d62045c016dbd91a6d2c6392281
ekgns33 <ekgns00421@gmail.com>
2024-01-09 04:27:11 +0000

    feat: define function for comparing list_elem in thread.h

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -28,6 +28,8 @@ typedef int tid_t;
 #define PRI_DEFAULT 31                  /* Default priority. */
 #define PRI_MAX 63                      /* Highest priority. */
 
+bool cmp_PRI(struct list_elem* a, struct list_elem* b, void* aux);
+
 /* A kernel thread or user process.
  *
  * Each thread structure is stored in its own 4 kB page.  The
